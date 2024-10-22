458f9cc9d2181f4fd8ca9ddbc1c2813c808714e8
StepanBambestikov <93329924+StepanBambestikov@users.noreply.github.com>
2024-10-22 11:22:07 +0700

    Add files via upload

---
+++ ansible/nginx_n_ssl/variables.tf
@@ -0,0 +1,30 @@
+
+
+variable "ttl_number"{
+
+}
+
+variable "my_ssh_key"{
+	
+}
+
+variable "rebrain_ssh_key"{
+
+}
+
+variable "do_token"{
+
+}
+variable "domain_name2"{}
+variable "module_tag"{}
+variable "input_domain_zone"{}
+variable "domain_name"{} 
+
+variable "aws_access_key"{
+  sensitive = true
+}
+
+variable "aws_secret_key"{
+  sensitive = true
+}
+
